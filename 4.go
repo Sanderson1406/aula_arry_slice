888a517e31e3af0e64d0f602fe5cf5796ca4776c
Sanderson <sandersonoficial10@hotmail.com>
2023-04-21 17:54:18 -0300

    01

--- 4.go
+++ 4.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var num = [6]int{5, 8, 11, 18, 20, 25}
+	var num = [6]float64{5.8, 8.5, 11.8, 18.11, 20.18, 25.20}
 	soma := 0
 	for i := 0; i < len(num); i++ {
 		soma += num[i]
--- 5.go
+++ 5.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var matriz [3][4]int
+	matriz := [3][4]int{{4, 5, 6, 7}, {7, 4, 1, 8}, {8, 5, 2, 3}}
 	for linha := range matriz {
 		for coluna := range matriz[linha] {
 			matriz[linha][coluna] = linha + coluna
 		}
+
 	}
 	fmt.Println(matriz)
 }
